6b3267e65102babbe106cd8b183aece45b93e420
Greg Johnston <greg.johnston@gmail.com>
2022-01-26 15:54:04 -0500

    Rotate weekday collects

--- episcopal_api/crates/library/src/rite2/office/morning_prayer.rs
+++ episcopal_api/crates/library/src/rite2/office/morning_prayer.rs
@@ -369,30 +369,38 @@ lazy_static! {
 
         Document::from(Content::CollectOfTheDay { allow_multiple: true }),
 
+        // Weekday collects: will rotate among the acceptable possibilities, by day
         Document::from(Text::from("O God, you make us glad with the weekly remembrance of the glorious resurrection of your Son our Lord: Give us this day such blessing through our worship of you, that the week to come may be spent in your favor; through Jesus Christ our Lord.")
-          .response("Amen."))
-          .label("A Collect for Sundays")
-          .condition(Condition::Weekday(Weekday::Sun)),
-        Document::from(Text::from("Almighty God, whose most dear Son went not up to joy but first he suffered pain, and entered not into glory before he was crucified: Mercifully grant that we, walking in the way of the cross, may find it none other than the way of life and peace; through Jesus Christ our Lord.")
-          .response("Amen."))
-          .label("A Collect for Fridays")
-          .condition(Condition::Weekday(Weekday::Fri)),
-        Document::from(Text::from("Almighty God, who after the creation of the world rested from all your works and sanctified a day of rest for all your creatures: Grant that we, putting away all earthly anxieties, may be duly prepared for the service of your sanctuary, and that our rest here upon earth may be a preparation for the eternal rest promised to your people in heaven; through Jesus Christ our Lord.")
-          .response("Amen."))
-          .label("A Collect for Saturdays")
-          .condition(Condition::Weekday(Weekday::Sat)),
-        Document::from(Text::from("O God, the King eternal, whose light divides the day from the night and turns the shadow of death into the morning: Drive far from us all wrong desires, incline our hearts to keep your law, and guide our feet into the way of peace; that, having done your will with cheerfulness during the day, we may, when night comes, rejoice to give you thanks; through Jesus Christ our Lord.")
-          .response("Amen."))
-          .label("A Collect for the Renewal of Life"),
-        Document::from(Text::from("O God, the author of peace and lover of concord, to know you is eternal life and to serve you is perfect freedom: Defend us, your humble servants, in all assaults of our enemies; that we, surely trusting in your defense, may not fear the power of any adversaries; through the might of Jesus Christ our Lord. ")
-          .response("Amen."))
-          .label("A Collect for Peace"),
-        Document::from(Text::from("Lord God, almighty and everlasting Father, you have brought us in safety to this new day: Preserve us with your mighty power, that we may not fall into sin, nor be overcome by adversity; and in all we do, direct us to the fulfilling of your purpose; through Jesus Christ our Lord.")
-          .response("Amen."))
-          .label("A Collect for Grace"),
-        Document::from(Text::from("Heavenly Father, in you we live and move and have our being: We humbly pray you so to guide and govern us by your Holy Spirit, that in all the cares and occupations of our life we may not forget you, but may remember that we are ever walking in your sight; through Jesus Christ our Lord. ")
-          .response("Amen."))
-          .label("A Collect for Guidance"),
+            .response("Amen."))
+            .label("A Collect for Sundays")
+            .condition(Condition::Weekday(Weekday::Sun)),
+          Document::from(Text::from("Almighty God, whose most dear Son went not up to joy but first he suffered pain, and entered not into glory before he was crucified: Mercifully grant that we, walking in the way of the cross, may find it none other than the way of life and peace; through Jesus Christ our Lord.")
+            .response("Amen."))
+            .label("A Collect for Fridays")
+            .condition(Condition::Weekday(Weekday::Fri)),
+          Document::from(Text::from("Almighty God, who after the creation of the world rested from all your works and sanctified a day of rest for all your creatures: Grant that we, putting away all earthly anxieties, may be duly prepared for the service of your sanctuary, and that our rest here upon earth may be a preparation for the eternal rest promised to your people in heaven; through Jesus Christ our Lord.")
+            .response("Amen."))
+            .label("A Collect for Saturdays")
+            .condition(Condition::Weekday(Weekday::Sat)),
+        Document::from(Choice::from([
+          Document::from(Text::from("O God, the King eternal, whose light divides the day from the night and turns the shadow of death into the morning: Drive far from us all wrong desires, incline our hearts to keep your law, and guide our feet into the way of peace; that, having done your will with cheerfulness during the day, we may, when night comes, rejoice to give you thanks; through Jesus Christ our Lord.")
+            .response("Amen."))
+            .label("A Collect for the Renewal of Life"),
+          Document::from(Text::from("O God, the author of peace and lover of concord, to know you is eternal life and to serve you is perfect freedom: Defend us, your humble servants, in all assaults of our enemies; that we, surely trusting in your defense, may not fear the power of any adversaries; through the might of Jesus Christ our Lord. ")
+            .response("Amen."))
+            .label("A Collect for Peace"),
+          Document::from(Text::from("Lord God, almighty and everlasting Father, you have brought us in safety to this new day: Preserve us with your mighty power, that we may not fall into sin, nor be overcome by adversity; and in all we do, direct us to the fulfilling of your purpose; through Jesus Christ our Lord.")
+            .response("Amen."))
+            .label("A Collect for Grace"),
+          Document::from(Text::from("Heavenly Father, in you we live and move and have our being: We humbly pray you so to guide and govern us by your Holy Spirit, that in all the cares and occupations of our life we may not forget you, but may remember that we are ever walking in your sight; through Jesus Christ our Lord. ")
+            .response("Amen."))
+            .label("A Collect for Guidance")
+        ]).should_rotate()).condition(Condition::None(vec![
+          Condition::Weekday(Weekday::Fri),
+          Condition::Weekday(Weekday::Sat),
+          Condition::Weekday(Weekday::Sun)
+        ])),
+
         Document::from(Rubric::from("Then, unless the Eucharist or a form of general intercession is to follow, one of these prayers for mission is added")),
         Document::from(Choice::from([
           Document::from(Text::from("O God, you have made of one blood all the peoples of the earth, and sent your blessed Son to preach peace to those who are far off and to those who are near: Grant that people everywhere may seek after you and find you; bring the nations into your fold; pour out your Spirit upon all flesh; and hasten the coming of your kingdom; through Jesus Christ our Lord. ")
