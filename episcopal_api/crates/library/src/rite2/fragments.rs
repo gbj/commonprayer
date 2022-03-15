5b3ba0e81d42d711282227d9f819e70b362dc9d1
Greg Johnston <greg.johnston@gmail.com>
2022-03-14 22:21:49 -0400

    Regularize spacing in Gloria Patri

--- episcopal_api/crates/library/src/rite2/fragments.rs
+++ episcopal_api/crates/library/src/rite2/fragments.rs
@@ -9,7 +9,7 @@ lazy_static! {
     pub static ref GLORIA_PATRI: GloriaPatri = GloriaPatri::from((
       "Glory to the Father, and to the Son, ",
       "and to the Holy Spirit: ",
-      "as it was in the beginning, is now,",
+      "as it was in the beginning, is now, ",
       "and will be for ever. Amen. "
     ));
 
