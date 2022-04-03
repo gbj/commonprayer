377373606487b3c623dc3f5653458edca28e66f1
Greg Johnston <greg.johnston@gmail.com>
2022-04-03 17:01:54 -0400

    Responsive prayer type conversions

--- episcopal_api/crates/liturgy/src/preces.rs
+++ episcopal_api/crates/liturgy/src/preces.rs
@@ -2,8 +2,10 @@ use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
+use crate::{Content, Litany, ResponsivePrayer, Text};
+
 /// An explanatory sentence or direction for the liturgy
-#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Preces(Vec<(String, String)>);
 
 impl Preces {
@@ -27,6 +29,67 @@ where
             source
                 .into_iter()
                 .map(|(a, b)| (a.to_string(), b.to_string()))
+                .collect(),
+        )
+    }
+}
+
+// Conversions
+impl From<Content> for Preces {
+    fn from(content: Content) -> Self {
+        match content {
+            Content::Preces(c) => c,
+            Content::Text(c) => Self::from(c),
+            Content::Litany(c) => Self::from(c),
+            Content::ResponsivePrayer(c) => Self::from(c),
+            _ => Self::default(),
+        }
+    }
+}
+
+impl From<Text> for Preces {
+    fn from(content: Text) -> Self {
+        Self(
+            content
+                .text
+                .split('\n')
+                .map(|line| {
+                    let mut parts = line.split(' ');
+                    (
+                        parts.next().unwrap_or_default().to_string(),
+                        parts.next().unwrap_or_default().to_string(),
+                    )
+                })
+                .collect(),
+        )
+    }
+}
+
+impl From<ResponsivePrayer> for Preces {
+    fn from(content: ResponsivePrayer) -> Self {
+        Self(
+            content
+                .into_vec()
+                .into_iter()
+                .map(|line| (String::new(), line))
+                .collect(),
+        )
+    }
+}
+
+impl From<Litany> for Preces {
+    fn from(content: Litany) -> Self {
+        let response = content.response.clone();
+        Self(
+            content
+                .into_vec()
+                .into_iter()
+                .flat_map({
+                    move |line| {
+                        std::iter::once((String::new(), line))
+                            .chain(std::iter::once((String::new(), response.clone())))
+                    }
+                })
                 .collect(),
         )
     }
--- episcopal_api/crates/liturgy/src/responsive_prayer.rs
+++ episcopal_api/crates/liturgy/src/responsive_prayer.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 
+use crate::{Content, Litany, Preces, Text};
+
 /// A simple responsive prayer in which the leader and participants alternate.
-#[derive(Clone, Debug, Hash, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Hash, Eq, PartialEq, Serialize, Deserialize)]
 pub struct ResponsivePrayer(Vec<String>);
 
 impl ResponsivePrayer {
@@ -22,5 +24,43 @@ where
 {
     fn from(content: T) -> Self {
         Self(content.into_iter().map(|line| line.to_string()).collect())
+    }
+}
+
+// Conversions
+impl From<Content> for ResponsivePrayer {
+    fn from(content: Content) -> Self {
+        match content {
+            Content::ResponsivePrayer(c) => c,
+            Content::Litany(c) => Self::from(c),
+            Content::Preces(c) => Self::from(c),
+            Content::Text(c) => Self::from(c),
+            _ => Self::default(),
+        }
+    }
+}
+
+impl From<Litany> for ResponsivePrayer {
+    fn from(content: Litany) -> Self {
+        let response = content.response.clone();
+        Self(
+            content
+                .into_vec()
+                .into_iter()
+                .flat_map(|line| std::iter::once(line).chain(std::iter::once(response.clone())))
+                .collect(),
+        )
+    }
+}
+
+impl From<Preces> for ResponsivePrayer {
+    fn from(content: Preces) -> Self {
+        Self(content.into_vec().into_iter().map(|(_, r)| r).collect())
+    }
+}
+
+impl From<Text> for ResponsivePrayer {
+    fn from(content: Text) -> Self {
+        Self(content.text.split('\n').map(String::from).collect())
     }
 }
