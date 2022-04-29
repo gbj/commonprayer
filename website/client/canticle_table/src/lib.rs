5d8cfa903b8d5b88d7ac1c78b93a0a419374472b
Greg Johnston <greg.johnston@gmail.com>
2022-04-29 11:46:25 -0400

    Canticle Table page is static

--- website/client/canticle_table/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use leptos::hydrate_page;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
-use website::pages::canticle_table;
-
-#[wasm_bindgen]
-pub fn hydrate_canticle_table(locale: String, serialized_state: JsValue) -> Result<(), JsValue> {
-    hydrate_page(canticle_table, &locale, serialized_state)
-}
